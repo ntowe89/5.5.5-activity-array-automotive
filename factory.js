e24f1ee4fff04ab08a7d7a4b48894e325775c849
Nathan Towe <nathanwtowe89@gmail.com>
2023-09-11 18:34:23 -0400

    finished

--- factory.js
+++ factory.js
@@ -2,16 +2,17 @@ class Factory{
     constructor(location, owner){
         this.location = location;
         this.owner = owner;
+        this.vehicles = [];
     }
-    createVehicle(Vehicle){
-
-    }
-    listVehicles(){
-        
+    createVehicle(vehicle){
+        this.vehicles.push(vehicle);
     }
     paintVehicle(vin, paintColor){
-
+        let vIndex = this.vehicles.findIndex(v => v.vin === vin)
+        this.vehicles[vIndex].paint(paintColor)
+    }
+    listVehicles(){
+        return this.vehicles;
     }
 }
 
-const german = new Factory("Germany", "Elon Tusk")
--- vehicle.js
+++ vehicle.js
@@ -1,24 +1,21 @@
 class Vehicle {
-    constructor(make, model, year, price, vin, color){
+    constructor(make, model, year, price, vin){
         this.make = make;
         this.model = model;
         this.year = year;
         this.price = price;
         this.vin = vin;
-        this.color = color;
+        this.color = undefined;
     }
 
     drive(){
-        console.log(this.year + ' ' + this.make + ' ' + this.model + ' drives')
+        return this.year + ' ' + this.make + ' ' + this.model + ' drives'
     }
     paint(color){
         this.color = color;
     }
 }
 
-const v = new Vehicle("Jord", "J-051", 2015, 50000, "12312", undefined);
 
-v.paint("blue");
-v.drive();
-console.log(v);
+
 
